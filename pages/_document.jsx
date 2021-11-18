3c1a721180c09d7c740fcf9f2669330492a7496f
Messias Martins <messiasmartins@outlook.com>
2021-11-18 05:58:05 -0300

    Alteração na Linguagem Padrão (en-Us/pt-BR)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-BR">
         <Head>
           <link rel="favicon.ico" sizes="76x76" href="/static/favicons/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.ico" />
