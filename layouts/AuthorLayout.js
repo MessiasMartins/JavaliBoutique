70f8410d6cc451dd68b3a82d66e436b6003902de
rfael5 <rfr9@protonmail.com>
2021-12-19 10:19:05 -0300

    Ajustando posicionamento de elementos na página

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -12,7 +12,7 @@ export default function AuthorLayout({ children, frontMatter }) {
         title={`Sobre - ${siteMetadata.otherTitle}`}
         description={siteMetadata.description}
       />
-      <div className="divide-y">
+      <div className="divide-y max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Sobre
--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -18,7 +18,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
 
   return (
     <>
-      <div className="divide-y">
+      <div className="divide-y max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
