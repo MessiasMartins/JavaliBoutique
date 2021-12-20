f882c10cfa9998443d5897ef8edc9bdad18ef0cb
rfael5 <rfr9@protonmail.com>
2021-12-20 15:09:34 -0300

    Ajustando posicionamento do titulo no ListLayout

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -19,7 +19,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   return (
     <>
       <div className="divide-y">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5 ml-32">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5 ml-0 lg:ml-32">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
