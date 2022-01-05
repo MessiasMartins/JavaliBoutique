561dc6204ecd101d1acf25f861be2d4e226184ab
rfael5 <rfr9@protonmail.com>
2022-01-05 16:10:39 -0300

    Colocando logomarca no responsivo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -16,7 +16,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label="Tailwind CSS Blog">
               <div className="flex items-center justify-between">
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block my-auto">
+                  <div className="h-6 text-2xl font-semibold sm:block my-auto">
                     <img alt src="/static/images/JAVALI-LOGO2.png"></img>
                   </div>
                 ) : (
