bdd40a354c9b6f72abee82c0eb5a619529498efa
rfael5 <rfr9@protonmail.com>
2022-01-29 17:08:47 -0300

    Concertando indentação

--- components/MovieRow.js
+++ components/MovieRow.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import productsData from '@/data/productsData';
-import Card from './Card';
+import React, { useState } from 'react'
+import productsData from '@/data/productsData'
+import Card from './Card'
 import { ArrowCircleRightIcon } from '@heroicons/react/solid'
 import { ArrowCircleLeftIcon } from '@heroicons/react/solid'
 import { ChevronRightIcon } from '@heroicons/react/solid'
@@ -10,71 +10,44 @@ import { ChevronLeftIcon } from '@heroicons/react/solid'
 //import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 //import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
-export default ({title,items}) => {
+export default ({ title, items}) => {
 
-    const [scrollX, setScrollX] = useState(0);
-
+    const [scrollX, setScrollX] = useState(0)
     let avanco = 500
-
-   
-
-    
-
     //Configurando as setas para rolar os filmes na lista
     const handleLeftArrow = () => {
-
-        let x = scrollX + Math.round(window.innerWidth / 2);
+        let x = scrollX + Math.round(window.innerWidth / 2)
         if(x > 0) {
             x = 0
         };
         setScrollX(x)
-
     }
-
-    const handleRightArrow = () => {
-
-               
-        
-        let x = scrollX - Math.round(window.innerWidth / 2);
-
-        let listW = productsData.length * 380;        
-        
+    const handleRightArrow = () => {       
+        let x = scrollX - Math.round(window.innerWidth / 2)
+        let listW = productsData.length * 380       
         if(window.innerWidth < 1200){
-            listW = productsData.length * 380;
-        }        
-
+            listW = productsData.length * 380}
         if(window.innerWidth < 500){
-            listW = productsData.length * 380;
-        }
-
+            listW = productsData.length * 380}
         if((window.innerWidth - listW) > x) {
-            x = (window.innerWidth - listW) - 60
-        }
-        setScrollX(x);
-
+            x = (window.innerWidth - listW) - 60}
+        setScrollX(x)
     }
-
     return (
         <div className="movieRow">
-            <h2>{title}</h2>
-            
+            <h2>{title}</h2>            
             <div className="seta-esquerda" onClick={handleLeftArrow}>
                 <ChevronLeftIcon className="h-24 w-24 text-gray-400" />
             </div>
-
             <div className="seta-direita" onClick={handleRightArrow}>
                 <ChevronRightIcon className="h-24 w-24 text-gray-400" />
             </div>
-
             <div className="overflow-x-hidden md:pl-20">
-
                 <div className="roll-5" style={{
                     marginLeft: scrollX,
                     width: productsData.length * avanco,
                 }}>
-
                 {productsData.map((produto) => (
-
                     <div className="inline-block ">
                         <Card                            
                             key={produto.title}
@@ -83,12 +56,10 @@ export default ({title,items}) => {
                             imgSrc={produto.imgSrc}
                             href={produto.href}
                         />
-                    </div>                
-                                        
+                    </div>                                       
                 ))}
                 </div>               
-
             </div>
         </div>
-    );
+    )
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,14 +17,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;1,500&display=swap" rel="stylesheet"></link>
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;1,500&display=swap" 
+            rel="stylesheet" 
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
             integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
             crossOrigin="anonymous"
-          />
-        
+          />        
         </Head>
         <body className="antialiased dark:bg-gray-900 text-gray-900 dark:text-white">
           <Main />
