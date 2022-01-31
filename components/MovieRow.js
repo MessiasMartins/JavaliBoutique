53c65c1f5b23f574d1d4b0d28dc3fe08aa10be9a
Messias Martins <messiasmartins@outlook.com>
2022-01-31 08:14:35 -0300

    Correções diversas para Deploy

--- components/MovieRow.js
+++ components/MovieRow.js
@@ -6,60 +6,74 @@ import { ArrowCircleLeftIcon } from '@heroicons/react/solid'
 import { ChevronRightIcon } from '@heroicons/react/solid'
 import { ChevronLeftIcon } from '@heroicons/react/solid'
 
-
 //import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 //import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
-export default ({ title, items}) => {
-
-    const [scrollX, setScrollX] = useState(0)
-    let avanco = 500
-    //Configurando as setas para rolar os filmes na lista
-    const handleLeftArrow = () => {
-        let x = scrollX + Math.round(window.innerWidth / 2)
-        if(x > 0) {
-            x = 0
-        };
-        setScrollX(x)
+export default function MovieRow(title, button, index) {
+  const [scrollX, setScrollX] = useState(0)
+  let avanco = 500
+  //Configurando as setas para rolar os filmes na lista
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2)
+    if (x > 0) {
+      x = 0
     }
-    const handleRightArrow = () => {       
-        let x = scrollX - Math.round(window.innerWidth / 2)
-        let listW = productsData.length * 380       
-        if(window.innerWidth < 1200){
-            listW = productsData.length * 380}
-        if(window.innerWidth < 500){
-            listW = productsData.length * 380}
-        if((window.innerWidth - listW) > x) {
-            x = (window.innerWidth - listW) - 60}
-        setScrollX(x)
+    setScrollX(x)
+  }
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2)
+    let listW = productsData.length * 380
+    if (window.innerWidth < 1200) {
+      listW = productsData.length * 380
     }
-    return (
-        <div className="movieRow">
-            <h2>{title}</h2>            
-            <div className="seta-esquerda" onClick={handleLeftArrow}>
-                <ChevronLeftIcon className="h-24 w-24 text-gray-400" />
+    if (window.innerWidth < 500) {
+      listW = productsData.length * 380
+    }
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60
+    }
+    setScrollX(x)
+  }
+  return (
+    <div className="movieRow" onKeyDown={this.handleClic} role={button}>
+      <h2>{title}</h2>
+      <div
+        className="seta-esquerda"
+        onClick={handleLeftArrow}
+        onKeyDown={this.handleClic}
+        role={button}
+      >
+        <ChevronLeftIcon className="h-24 w-24 text-gray-400" />
+      </div>
+      <div
+        className="seta-direita"
+        onClick={handleRightArrow}
+        onKeyDown={this.handleClic}
+        role={button}
+      >
+        <ChevronRightIcon className="h-24 w-24 text-gray-400" />
+      </div>
+      <div className="overflow-x-hidden md:pl-20">
+        <div
+          className="roll-5"
+          style={{
+            marginLeft: scrollX,
+            width: productsData.length * avanco,
+          }}
+        >
+          {productsData.map((produto) => (
+            <div className="inline-block " key={index}>
+              <Card
+                key={produto.title}
+                title={produto.title}
+                description={produto.description}
+                imgSrc={produto.imgSrc}
+                href={produto.href}
+              />
             </div>
-            <div className="seta-direita" onClick={handleRightArrow}>
-                <ChevronRightIcon className="h-24 w-24 text-gray-400" />
-            </div>
-            <div className="overflow-x-hidden md:pl-20">
-                <div className="roll-5" style={{
-                    marginLeft: scrollX,
-                    width: productsData.length * avanco,
-                }}>
-                {productsData.map((produto) => (
-                    <div className="inline-block ">
-                        <Card                            
-                            key={produto.title}
-                            title={produto.title}
-                            description={produto.description}
-                            imgSrc={produto.imgSrc}
-                            href={produto.href}
-                        />
-                    </div>                                       
-                ))}
-                </div>               
-            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
