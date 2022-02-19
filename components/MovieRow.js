adc673bca4aef92162b5e0b87185a5d7822cceee
Messias Martins <messiasmartins@outlook.com>
2022-02-19 10:09:45 -0300

    Correção Deploy

--- components/MovieRow.js
+++ components/MovieRow.js
@@ -32,12 +32,12 @@ export default function MovieRow(title, button, index) {
     setScrollX(x)
   }
   return (
-    <div className="movieRow" onKeyDown={this.handleClic} role={button}>
+    <div className="movieRow" onKeyDown={this.handleClick} role={button}>
       <h2>{title}</h2>
       <div
         className="seta-esquerda"
         onClick={handleLeftArrow}
-        onKeyDown={this.handleClic}
+        onKeyDown={this.handleClick}
         role={button}
       >
         <ChevronLeftIcon className="h-24 w-24 text-gray-400" />
@@ -45,7 +45,7 @@ export default function MovieRow(title, button, index) {
       <div
         className="seta-direita"
         onClick={handleRightArrow}
-        onKeyDown={this.handleClic}
+        onKeyDown={this.handleClick}
         role={button}
       >
         <ChevronRightIcon className="h-24 w-24 text-gray-400" />
